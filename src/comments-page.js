5b61ee7eb2be1ceb581638d32b6b2ca78a3c4f96
Daria <kattdaria@gmail.com>
2018-02-03 16:29:01 +0200

    Add form to comments page

--- src/comments-page.js
+++ src/comments-page.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { withRouter } from 'react-router';
+import { withStyles } from 'material-ui/styles';
 import CommentsList from './comments-list';
+import CommentsForm from './comments-form';
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    justifyContent: 'space-between',
+    height: '100vh',
+  },
+};
 
 class CommentsPage extends Component {
   constructor(props) {
@@ -15,12 +26,14 @@ class CommentsPage extends Component {
     const item = this.props.items.filter(i => i.id === this.state.id)[0];
     if (!item) return <Redirect to="/" />;
     const { comments } = item;
+    const { classes } = this.props;
     return (
-      <div>
-        {comments && <CommentsList comments={comments} /> }
+      <div className={classes.container}>
+        <CommentsForm save={text => this.props.save(this.state.id, text)} />
+        {comments && <CommentsList comments={comments} />}
       </div>
     );
   }
 }
 
-export default withRouter(CommentsPage);
+export default withStyles(styles)(withRouter(CommentsPage));
